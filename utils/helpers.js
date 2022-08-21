006178fbb24c7b0b905ba8a603abd728627131b6
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2022-08-21 15:40:48 -0700

    format date_time helper function, format_amount updated

---
+++ utils/helpers.js
@@ -0,0 +1,10 @@
+// format date_time
+module.exports = {
+    format_date: (date) => {
+        return `${new Date(date).getDate()}/${new Date(date).getMonth() + 1}/${new Date(date).getFullYear()}`;
+    },
+    format_amount: (amount) => {
+        // format large numbers with commas
+        return parseInt(amount).toLocaleString();
+    }
+};
