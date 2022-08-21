9890c346982223ed54644f909dccc3e375f9f8eb
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2022-08-21 15:35:00 -0700

    utils userAuth helper function updated

---
+++ utils/userAuth.js
@@ -0,0 +1,10 @@
+// redirect user to login route, if user is not logged in
+const withAuth = (req, res, next) => {
+    if (!req.session.logged_in) {
+        res.redirect('/login');
+    } else {
+        next();
+    }
+};
+
+module.exports = withAuth;
